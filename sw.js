bd81aa7c5d8a6f53cd05e44d3d461753b003739c
Roccella <roccellapablo@gmail.com>
2026-09-06 15:46:17 -0300

    deploy: tudux@2936f98

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = '4be7e0b701';
+const SW_VERSION = 'ddbad65bf2';
 // El SW se copia verbatim desde public/, así que Vite no le reescribe nada con base.
 // La saca de su propia URL: '/' en prod, '/tudux-demo/' servido bajo ese subpath.
 const BASE = new URL('./', self.location).pathname;
